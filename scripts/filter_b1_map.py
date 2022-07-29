dbf5a5c7ae12f942f62b35ea4c85a5528d3730bb
Julien Cohen-Adad <jcohen@polymtl.ca>
2022-07-29 10:04:36 -0400

    Added 3D Gaussian filtering

--- scripts/filter_b1_map.py
+++ scripts/filter_b1_map.py
@@ -12,6 +12,11 @@
 
 import numpy as np
 import nibabel as nib
+from astropy.convolution import convolve
+
+from gaussian3d import Gaussian3DKernel
+
+
 # load images
 nii_mask = nib.load('acdc_spine_7t_037_48_T0000_th15.nii.gz')
 nii_data = nib.load('acdc_spine_7t_037_56_T0000.nii.gz')
@@ -21,12 +26,10 @@ ind_zero = np.where(nii_mask.get_fdata() == 0.0)
 data_nan = data.copy()
 data_nan[ind_zero] = np.nan
 
-# The code below is copied from astropy's tutorial and works for 2D but not for 3D. I'm putting it here to help prototyping a new class.
-# from astropy.convolution import convolve
-# from astropy.convolution import Gaussian2DKernel  # ideally we would create a new class Gaussian3DKernel
-# kernel = Gaussian2DKernel(x_stddev=1)
-# data_nan_astropy_conv = convolve(data_nan, kernel)
+# 3D Gaussian filtering, ignoring NaN
+kernel = Gaussian3DKernel(stddev=1)
+data_filt = convolve(data_nan, kernel)
 
 # Save data
-nii_data_filt = nib.nifti1.Nifti1Image(data_nan, nii_data.affine)
-nib.save(nii_data_filt, 'bla.nii.gz')
+nii_data_filt = nib.nifti1.Nifti1Image(data_filt, nii_data.affine)
+nib.save(nii_data_filt, 'bla_filt.nii.gz')
