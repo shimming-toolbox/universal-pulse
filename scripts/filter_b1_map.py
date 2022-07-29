7ce7fbe900b15e6f7f734bb4bf93673678a9f30d
Julien Cohen-Adad <jcohen@polymtl.ca>
2022-07-29 09:23:10 -0400

    Added script to filter B1 maps

---
+++ scripts/filter_b1_map.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+# Filter B1 map.
+# 
+# B1 maps are based on the B1+ interferometry sequence from Neurospin/CEA.
+#
+# ------------------------------------------------------------------------------
+# Copyright (c) 2022 Polytechnique Montreal <www.neuro.polymtl.ca>
+# Author: Julien Cohen-Adad
+# Licence: https://github.com/shimming-toolbox/universal-pulse/blob/main/LICENSE
+
+
+import numpy as np
+import nibabel as nii
+# load images
+nii_mask = nii.load('acdc_spine_7t_037_48_T0000_th15.nii.gz')
+nii_data = nii.load('acdc_spine_7t_037_56_T0000.nii.gz')
+data = nii_data.get_fdata().astype(np.float32)
+# convert masked voxels to nan
+ind_zero = np.where(nii_mask.get_fdata() == 0.0)
+data_nan = data.copy()
+data_nan[ind_zero] = np.nan
+
+# The code below is copied from astropy's tutorial and works for 2D but not for 3D. I'm putting it here to help prototyping a new class.
+from astropy.convolution import convolve
+from astropy.convolution import Gaussian2DKernel  # ideally we would create a new class Gaussian3DKernel
+kernel = Gaussian2DKernel(x_stddev=1)
+data_nan_astropy_conv = convolve(data_nan, kernel)
