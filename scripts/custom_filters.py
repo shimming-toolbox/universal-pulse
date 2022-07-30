73d522c2c328432a02873e002af00b5a5fec01f9
Mathieu Boudreau <emb6150@gmail.com>
2022-07-30 01:17:01 -0300

    Add docstring

--- scripts/custom_filters.py
+++ scripts/custom_filters.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 def median_func(x):
+    ''' Median func
+    Objective function for median filter
+
+    At edges defined by NaNs (eg, from mask), values outside the volume
+    are not counted in the median. This is to allow for better edge
+    preservation.
+    '''
+    
     x = x[~np.isnan(x)]
     if x.size==0:
         return np.nan 
