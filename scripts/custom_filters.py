fe891232e59cad62d39cee4bcf2450afcefb9321
Mathieu Boudreau <emb6150@gmail.com>
2022-08-02 16:07:09 -0300

    PEP8

--- scripts/custom_filters.py
+++ scripts/custom_filters.py
@@ -2,24 +2,26 @@ import numpy as np
 import scipy.ndimage as spi
 import nibabel as nib
 
+
 def median_func(x):
-    ''' Median func
+    """ Median func
     Objective function for median filter
 
     At edges defined by NaNs (eg, from mask), values outside the volume
     are not counted in the median. This is to allow for better edge
     preservation.
-    '''
-    
+    """
+
     x = x[~np.isnan(x)]
-    if x.size==0:
-        return np.nan 
+    if x.size == 0:
+        return np.nan
     else:
-        x=np.median(x)
+        x = np.median(x)
         return x
 
+
 def median_filter(img, mask=None, kernel_size=5):
-    ''' Median filter for NIFTI volume.
+    """ Median filter for NIFTI volume.
     Application: B1+ map smoothing.
 
     img: NIFTI volume to be smoothed, in a Nibabel object.
@@ -29,7 +31,7 @@ def median_filter(img, mask=None, kernel_size=5):
         slice thickness is different from in-plane resolution.
 
     return: Filtered and masked img.
-    '''
+    """
 
     data = img.get_fdata().astype(np.float32)
 
@@ -42,15 +44,15 @@ def median_filter(img, mask=None, kernel_size=5):
         data_mask[ind_zero] = np.nan
 
     # Get image voxel dim info
-    pixdim = img.header['pixdim'][1:4]
+    pixdim = img.header["pixdim"][1:4]
 
     # Setup filter "footprint", ndarray where median is going to be calculated around the pixel
     # of interest. Round up to integers when calculating array dims.
-    norm_dim = pixdim/min(pixdim)
-    kernel_size = np.ndarray.astype(np.ceil(kernel_size/(norm_dim)), dtype=int)
-    footprint= np.ones(kernel_size) 
+    norm_dim = pixdim / min(pixdim)
+    kernel_size = np.ndarray.astype(np.ceil(kernel_size / (norm_dim)), dtype=int)
+    footprint = np.ones(kernel_size)
 
-    data_filt=spi.generic_filter(data_mask, median_func, footprint=footprint)
+    data_filt = spi.generic_filter(data_mask, median_func, footprint=footprint)
 
     if mask is not None:
         data_filt[ind_zero] = np.nan
